613cea16ea7767a8b7cfaedf6c9f202f1723b9c3
Eric Mignot <eric.mignot@gmail.com>
2020-07-24 11:37:56 -0700

    merge without conflict

---
+++ demos/5.branch/merge-without-conflict.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+function test_merge_does_not_commit {
+    cd /usr/local/src/demos/5.branch
+    rm -rf server
+    rm -rf clients
+
+    svnadmin create server
+    svn mkdir file:///usr/local/src/demos/5.branch/server/trunk -m "trunk created"
+    
+    mkdir -p clients/bob
+    cd clients/bob
+    svn checkout file:///usr/local/src/demos/5.branch/server/trunk .    
+
+    echo "hello trunk" > trunk.txt
+    svn add trunk.txt
+    svn commit -m "file added in trunk"
+
+    svn copy --parents ^/trunk ^/branches/one -m "branched from trunk"
+
+    echo "modified" >> trunk.txt
+    svn commit -m "file modified in trunk"
+
+    svn switch ^/branches/one
+    echo "hello branch" > branch.txt
+    svn add branch.txt
+    svn commit -m "file added in branch"    
+
+    svn switch ^/trunk
+    svn merge ^/branches/one
+    local isDirty=`svn status | grep "A  +    branch.txt" | wc -l`
+
+    assertequals $isDirty 1
+}
+
+function test_merge_hides_branch_commits {
+    cd /usr/local/src/demos/5.branch
+    rm -rf server
+    rm -rf clients
+
+    svnadmin create server
+    svn mkdir file:///usr/local/src/demos/5.branch/server/trunk -m "trunk created"
+    
+    mkdir -p clients/bob
+    cd clients/bob
+    svn checkout file:///usr/local/src/demos/5.branch/server/trunk .    
+
+    echo "hello trunk" > trunk.txt
+    svn add trunk.txt
+    svn commit -m "file added in trunk"
+
+    svn copy --parents ^/trunk ^/branches/one -m "branched from trunk"
+
+    echo "modified" >> trunk.txt
+    svn commit -m "file modified in trunk"
+
+    svn switch ^/branches/one
+    echo "hello branch" > branch.txt
+    svn add branch.txt
+    svn commit -m "file added in branch"    
+
+    svn switch ^/trunk
+    svn merge ^/branches/one
+    svn commit -m "branch merged"
+
+    assertequals "$(svn log ^/trunk | revision_list)" "r6-r4-r2-r1"
+}
