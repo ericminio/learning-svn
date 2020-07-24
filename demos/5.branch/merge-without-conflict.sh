a8bbe1835c69b357b6ff539dc63aa7223ffce739
Eric Mignot <eric.mignot@gmail.com>
2020-07-24 11:44:53 -0700

    log after merge

--- demos/5.branch/merge-without-conflict.sh
+++ demos/5.branch/merge-without-conflict.sh
@@ -33,7 +33,7 @@ function test_merge_does_not_commit {
     assertequals $isDirty 1
 }
 
-function test_merge_hides_branch_commits {
+function test_log_hides_merged_commits {
     cd /usr/local/src/demos/5.branch
     rm -rf server
     rm -rf clients
@@ -64,4 +64,37 @@ function test_merge_hides_branch_commits {
     svn commit -m "branch merged"
 
     assertequals "$(svn log ^/trunk | revision_list)" "r6-r4-r2-r1"
+}
+
+function test_log_can_show_merged_commits {
+    cd /usr/local/src/demos/5.branch
+    rm -rf server
+    rm -rf clients
+
+    svnadmin create server
+    svn mkdir file:///usr/local/src/demos/5.branch/server/trunk -m "trunk created"
+    
+    mkdir -p clients/bob
+    cd clients/bob
+    svn checkout file:///usr/local/src/demos/5.branch/server/trunk .    
+
+    echo "hello trunk" > trunk.txt
+    svn add trunk.txt
+    svn commit -m "file added in trunk"
+
+    svn copy --parents ^/trunk ^/branches/one -m "branched from trunk"
+
+    echo "modified" >> trunk.txt
+    svn commit -m "file modified in trunk"
+
+    svn switch ^/branches/one
+    echo "hello branch" > branch.txt
+    svn add branch.txt
+    svn commit -m "file added in branch"    
+
+    svn switch ^/trunk
+    svn merge ^/branches/one
+    svn commit -m "branch merged"
+
+    assertequals "$(svn log --use-merge-history ^/trunk | revision_list)" "r6-r5-r3-r4-r2-r1"
 }
