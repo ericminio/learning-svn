51f2ab86d3e9de79fa0a3d52a181b5a1b9bf96d0
ericminio <eric.mignot@gmail.com>
2020-07-25 10:27:57 -0700

    branch can be deleted after merge

--- demos/4.branches/merge.sh
+++ demos/4.branches/merge.sh
@@ -7,10 +7,10 @@ function test_merge_does_not_commit {
 
     svnadmin create server
     svn mkdir file:///usr/local/src/demos/4.branches/server/trunk -m "trunk created"
-    
+
     mkdir -p clients/bob
     cd clients/bob
-    svn checkout file:///usr/local/src/demos/4.branches/server/trunk .    
+    svn checkout file:///usr/local/src/demos/4.branches/server/trunk .
 
     echo "hello trunk" > trunk.txt
     svn add trunk.txt
@@ -24,7 +24,7 @@ function test_merge_does_not_commit {
     svn switch ^/branches/one
     echo "hello branch" > branch.txt
     svn add branch.txt
-    svn commit -m "file added in branch"    
+    svn commit -m "file added in branch"
 
     svn switch ^/trunk
     svn merge ^/branches/one
@@ -40,10 +40,10 @@ function test_log_hides_merged_commits {
 
     svnadmin create server
     svn mkdir file:///usr/local/src/demos/4.branches/server/trunk -m "trunk created"
-    
+
     mkdir -p clients/bob
     cd clients/bob
-    svn checkout file:///usr/local/src/demos/4.branches/server/trunk .    
+    svn checkout file:///usr/local/src/demos/4.branches/server/trunk .
 
     echo "hello trunk" > trunk.txt
     svn add trunk.txt
@@ -57,7 +57,7 @@ function test_log_hides_merged_commits {
     svn switch ^/branches/one
     echo "hello branch" > branch.txt
     svn add branch.txt
-    svn commit -m "file added in branch"    
+    svn commit -m "file added in branch"
 
     svn switch ^/trunk
     svn merge ^/branches/one
@@ -73,10 +73,10 @@ function test_log_can_show_merged_commits {
 
     svnadmin create server
     svn mkdir file:///usr/local/src/demos/4.branches/server/trunk -m "trunk created"
-    
+
     mkdir -p clients/bob
     cd clients/bob
-    svn checkout file:///usr/local/src/demos/4.branches/server/trunk .    
+    svn checkout file:///usr/local/src/demos/4.branches/server/trunk .
 
     echo "hello trunk" > trunk.txt
     svn add trunk.txt
@@ -90,11 +90,50 @@ function test_log_can_show_merged_commits {
     svn switch ^/branches/one
     echo "hello branch" > branch.txt
     svn add branch.txt
-    svn commit -m "file added in branch"    
+    svn commit -m "file added in branch"
 
     svn switch ^/trunk
     svn merge ^/branches/one
     svn commit -m "branch merged"
 
     assertequals "$(svn log --use-merge-history ^/trunk | revision_list)" "r6-r5-r3-r4-r2-r1"
+}
+
+function test_merged_branch_can_be_deleted {
+    cd /usr/local/src/demos/4.branches
+    rm -rf server
+    rm -rf clients
+
+    svnadmin create server
+    svn mkdir file:///usr/local/src/demos/4.branches/server/trunk -m "trunk created"
+
+    mkdir -p clients/bob
+    cd clients/bob
+    svn checkout file:///usr/local/src/demos/4.branches/server/trunk .
+
+    echo "hello trunk" > trunk.txt
+    svn add trunk.txt
+    svn commit -m "file added in trunk"
+
+    svn copy --parents ^/trunk ^/branches/one -m "branched from trunk"
+
+    echo "modified" >> trunk.txt
+    svn commit -m "file modified in trunk"
+
+    svn switch ^/branches/one
+    echo "hello branch" > branch.txt
+    svn add branch.txt
+    svn commit -m "file added in branch"
+
+    svn switch ^/trunk
+    svn merge ^/branches/one
+    svn commit -m "branch merged"
+
+    svn remove ^/branches/one -m "delete branch after merge"
+
+    local start=`svn log -g | grep -n r5 | cut -d':' -f1`
+    local end=`svn log -g | tail -n +$start | grep -n "\--" | cut -d':' -f1 | head -n 1`
+    local message=`svn log -g | tail -n +$start | head -n $end | commit_message`
+
+    assertequals "$message" "file added in branch"
 }
